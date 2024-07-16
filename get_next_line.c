464663d18c29b1d36c31d12d2a427ef57f491d49
P25 <pedritoc25@gmail.com>
2024-07-16 13:07:02 +0200

    psudecode para recortar la statica

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: pcabanas <pcabanas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 15:53:44 by pcabanas          #+#    #+#             */
-/*   Updated: 2024/07/02 20:41:26 by pcabanas         ###   ########.fr       */
+/*   Updated: 2024/07/16 13:05:54 by pcabanas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ bytes es el numero de caracteres que se leen en una pasada
 line es el texto que devuelve getNextLine
 buffer_text es el buffer, donde se encuentra lo que has leido del fd
 stable es la variable estática
-temp es la variable temporal
+temp es la variable temporal para liberar la estatica cuando hga strjoin
 */
 
 char	*get_next_line(int fd)
@@ -37,21 +37,26 @@ char	*get_next_line(int fd)
 		return (NULL);
 	while (bytes > 0)
 	{
-		temp = ft_calloc(1, sizeof(char));
 		bytes = read(fd, buffer_text, BUFFER_SIZE);
 		if(stable == NULL)
-			stable = ft_calloc(1, sizeof(char));
+			stable = ft_calloc(1, sizeof(char*));
 		//buscar '\n' en stable
+		temp = stable;
+		stable = ft_strjoin(stable, buffer_text);
+		free(temp);
+		temp = stable;
 		if (ft_strchr(stable, '\n'))
 		{
-			temp = ft_strchr(stable, '\n');
-			line = stable;
+			//funcion substr
+			line = ft_substr(stable, 0, ft_strchr(stable, '\n') - stable + 1);
+			
+			//recortar estatica
+			stable = subtring (stable, len line, len stable - len line);
+			
+			break;
 		}
-		else
-			stable = ft_strjoin(stable, buffer_text);
 		//temp = stable;
 		//stable = strjoin(temp, buffer_text);
-		stable = temp;
 		//si te devuelve puntero, para
 		free (temp);
 	}
@@ -63,11 +68,19 @@ char	*get_next_line(int fd)
 int	main(void) //COMENTAR
 {
 	int		fd;
-	//int		line;
+	char	*line;
 
 	fd = open("prueba.txt", O_RDONLY);
-	get_next_line(fd);
-//	printf("\n%i", line); //BORRAR
+	while (1)
+	{
+	line = get_next_line(fd);
+	printf("\n%s", line);
+	if (!line)
+		break ;
+	free (line);
+	line = NULL;
+	}
+ //BORRAR
 	close(fd);
 	return (0);
 }
